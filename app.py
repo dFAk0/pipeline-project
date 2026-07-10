b7d6cb9d735018c92b34483f706efbe6cb4d40fe
dFAk0 <furkanadiguzel0742@gmail.com>
2026-07-10 23:05:15 +0300

    Reapply "initial commit: Flask application, CI pipeline and Docker"
    This reverts commit 0869cb3733c3dd054b5d90c8cabfa68f0571a2ab.

---
+++ app.py
@@ -0,0 +1,13 @@
+from flask import Flask, jsonify
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return jsonify(message="Hello!")
+
+@app.route("/health")
+def health():
+    return jsonify(status="nok")
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
